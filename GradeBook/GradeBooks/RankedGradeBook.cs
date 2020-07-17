cd3d614e33fa728337cc485dcec468c90d5c19b1
SunilSahasrabuddhe <SunilSahasrabuddhe@users.noreply.github.com>
2020-07-17 19:24:48 -0400

    WIP

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,5 +8,14 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook: BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name) { Type = GradeBookType.Ranked; }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            if (averageGrade >= 80)
+                return 'A';
+            return 'F';
+        }
     }
 }
